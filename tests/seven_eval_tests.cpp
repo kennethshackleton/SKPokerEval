002a13c9d7a7b95eabb2913dc44ca84290c27c78
Ken <k314j159s@gmail.com>
2015-06-07 22:18:44 +0900

    On stack.

--- tests/seven_eval_tests.cpp
+++ tests/seven_eval_tests.cpp
@@ -3,8 +3,8 @@
 #include "../src/SevenEval.h"
 
 TEST(SevenEval, Regression) {
-  FiveEval const* five_eval = new FiveEval();
-  SevenEval const* seven_eval = new SevenEval();
+  FiveEval const five_eval;
+  SevenEval const seven_eval;
   for (int i=0; i<46; ++i) {
     for (int j=i+1; j<47; ++j) {
       for (int k=j+1; k<48; ++k) {
@@ -12,8 +12,8 @@ TEST(SevenEval, Regression) {
           for (int m=l+1; m<50; ++m) {
             for (int n=m+1; n<51; ++n) {
               for (int p=n+1; p<52; ++p) {
-                int const five_rank = five_eval->GetRank(i, j, k, l, m, n, p);
-                int const seven_rank = seven_eval->GetRank(i, j, k, l, m, n, p);
+                int const five_rank = five_eval.GetRank(i, j, k, l, m, n, p);
+                int const seven_rank = seven_eval.GetRank(i, j, k, l, m, n, p);
                 ASSERT_EQ(five_rank, seven_rank) << "Ranks at "
                                                  << i << ", "
                                                  << j << ", "
