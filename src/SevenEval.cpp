18382b4da02452674396bd7269c1b3bfa6eb01e0
Ken <k314j159s@gmail.com>
2016-10-22 23:23:15 +0100

    Inline SevenEval::GetRank.

--- src/SevenEval.cpp
+++ src/SevenEval.cpp
@@ -184,29 +184,3 @@ SevenEval::~SevenEval() {
   delete[] mRankPtr;
   delete[] mFlushRankPtr;
 }
-
-short unsigned SevenEval::GetRank(int const i, int const j, int const k,
-    int const l, int const m, int const n, int const p) const {
-  // Create a 7-card hand key by adding up each of the card keys.
-  uint_fast32_t key = mDeckcardsKey[i] + mDeckcardsKey[j] + mDeckcardsKey[k] +
-    mDeckcardsKey[l] + mDeckcardsKey[m] + mDeckcardsKey[n] + mDeckcardsKey[p];
-  // Tear off the flush check strip.
-  int_fast16_t const flush_suit = mFlushCheck[key & SUIT_BIT_MASK];
-  if (flush_suit == NOT_A_FLUSH) {
-    // Tear off the non-flush key strip, and look up the rank.
-    key >>= NON_FLUSH_BIT_SHIFT;
-    // Take key modulo the circumference.
-    return mRankPtr[(key < CIRCUMFERENCE_SEVEN ? key :
-                     key - CIRCUMFERENCE_SEVEN)];
-  }
-  // Generate a flush key, and look up the rank.
-  int flush_key = 0;
-  if (mDeckcardsSuit[i] == flush_suit) flush_key  = mDeckcardsFlush[i];
-  if (mDeckcardsSuit[j] == flush_suit) flush_key += mDeckcardsFlush[j];
-  if (mDeckcardsSuit[k] == flush_suit) flush_key += mDeckcardsFlush[k];
-  if (mDeckcardsSuit[l] == flush_suit) flush_key += mDeckcardsFlush[l];
-  if (mDeckcardsSuit[m] == flush_suit) flush_key += mDeckcardsFlush[m];
-  if (mDeckcardsSuit[n] == flush_suit) flush_key += mDeckcardsFlush[n];
-  if (mDeckcardsSuit[p] == flush_suit) flush_key += mDeckcardsFlush[p];
-  return mFlushRankPtr[flush_key];
-}
