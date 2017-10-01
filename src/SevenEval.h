36ff93f28ced08aa980bc0ea207732e9825b20f2
Kenneth <kennethshackleton@users.noreply.github.com>
2017-10-01 19:35:50 +0100

    Remove comment that is equally no longer applicable.

--- src/SevenEval.h
+++ src/SevenEval.h
@@ -42,7 +42,6 @@ public:
     if (TDoFlushCheck) {
       auto const suit = flush_check[key >> FLUSH_BIT_SHIFT];
       if (NOT_A_SUIT != suit) {
-        // Generate a flush key, and look up the rank.
         auto const * const s = suit_kronecker[suit];
         return flush_ranks[s[i] | s[j] | s[k] | s[m] | s[n] | s[p] | s[q]];
       }
